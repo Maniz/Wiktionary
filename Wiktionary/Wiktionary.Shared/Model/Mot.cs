170a302d08c700912fdb0c3d0cad6b283d4480f5
amaniago <anthony.maniago@viacesi.fr>
2014-12-05 09:04:04 +0100

    ajout du modele pour les mots

---
+++ Wiktionary/Wiktionary.Shared/Model/Mot.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace Wiktionary.Model
+{
+    /// <summary>
+    /// Classe Modele pour la gestion d'un mot
+    /// </summary>
+    public class Mot
+    {
+        [PrimaryKey, Column("valeur"), NotNull]
+        public string Valeur { get; set; }
+        [Column("definition"), NotNull]
+        public string Definition { get; set; }
+    }
+}
